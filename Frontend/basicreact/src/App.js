83803f6b080056f956b3c6189dd58d2e8de6d2aa
Shubhamchaurasiy <91664093+Shubhamchaurasiy@users.noreply.github.com>
2024-07-31 17:05:25 +0000

    hi this is all react commit

---
+++ Frontend/basicreact/src/App.js
@@ -0,0 +1,10 @@
+
+
+
+function App() {
+  return (
+    <h1>hello this is shubham</h1>
+  );
+}
+
+export default App;
---
+++ myproject/src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <h1>
+    hello this is shubham application
+
+   </h1>
+  )
+}
+
+export default App
